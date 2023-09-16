66d92d5c52c7a4590aaa35a1cafcaafc24505678
llamasaresavager <trevor@pertainium.com>
2023-09-16 17:38:39 -0400

    saves html and attachments to folder named by doc_id

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+import os
 from utils import get_urls_from_xls, download_from_njstart
 
-xls_file_path = '/home/localadmin/Desktop/njstartscrape/njstartscrape/bidSearchResults.xls'
-urlsToScrape = get_urls_from_xls(xls_file_path)
+def scrape_and_save_data(url_objs, download_directory):
+    """
+    Iterates over url_objs and for each object, creates a folder named after its doc_id. 
+    The contents of the folder are populated by the download_from_njstart function.
 
-for each s
-# print(urlsToScrape)
+    Args:
+    - url_objs (list): List of objects with keys 'url_to_scrape' and 'doc_id'.
+    - download_directory (str): Base directory where data should be saved.
+    
+    Returns:
+    None
+    """
+
+    # Iterate over each URL object
+    for obj in url_objs:
+        doc_id = obj['doc_id']
+        url_to_scrape = obj['url_to_scrape']
+
+        # Create a new directory for each doc_id
+        doc_id_directory = os.path.join(download_directory, doc_id)
+        if not os.path.exists(doc_id_directory):
+            os.makedirs(doc_id_directory)
+
+        # Download the data into the specific directory
+        download_from_njstart(url_to_scrape, download_directory=doc_id_directory)
+
+
+xls_file_path = '/home/localadmin/Desktop/njstartscrape/njstartscrape/bidSearchResults.xls'
+download_directory = '/home/localadmin/Desktop/njstartscrape'
+url_obj_to_scrape = get_urls_from_xls(xls_file_path)
+
+# Call the function to start the scraping and saving process
+scrape_and_save_data(url_obj_to_scrape, download_directory)
--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import xlrd
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+import os
+
+url = "https://www.njstart.gov/bso/external/bidDetail.sdo?docId=24DPP00924&external=true&parentUrl=close"
 
 # Constants for the browser setup
 BINARY_LOCATION = '/home/localadmin/Documents/chromiumdriver/chrome-linux64/chrome'
@@ -10,12 +13,12 @@ DOWNLOAD_DIRECTORY = '/home/localadmin/Desktop/njstartscrape'
 
 def download_from_njstart(url, binary_location=BINARY_LOCATION, download_directory=DOWNLOAD_DIRECTORY, close_browser_after=True):
     """
-    Downloads files from the given njstart URL.
+    Downloads files and saves the HTML from the given njstart URL.
 
     Args:
     - url (str): The URL to scrape.
     - binary_location (str): Location of the Chrome binary.
-    - download_directory (str): Directory where files should be downloaded.
+    - download_directory (str): Directory where files and HTML should be saved.
     - close_browser_after (bool, optional): Flag to decide whether to close the browser after downloading. Defaults to True.
 
     Returns:
@@ -36,19 +39,24 @@ def download_from_njstart(url, binary_location=BINARY_LOCATION, download_directo
     time.sleep(5)
 
     # Identify all relevant anchor tags with href containing "javascript:downloadFile("
-    links = browser.find_elements(by= By.XPATH, value='//a[contains(@href, "javascript:downloadFile(")]')
+    links = browser.find_elements(by=By.XPATH, value='//a[contains(@href, "javascript:downloadFile(")]')
 
     # Iterate over each link and click to initiate the download
     for link in links:
         link.click()
         time.sleep(2)  # Allow time for the download to initiate
 
+    # Save the HTML content of the page
+    html_content = browser.page_source
+    with open(os.path.join(download_directory, "page_source.html"), 'w', encoding='utf-8') as file:
+        file.write(html_content)
+
     if close_browser_after:
         browser.close()
 
 def get_urls_from_xls(xls_file_path):
-    # Variables
-    urlsToScrape = []
+    # Array to store the results
+    urls_to_scrape_data = []
 
     # Read the .xls file
     workbook = xlrd.open_workbook(xls_file_path)
@@ -56,8 +64,9 @@ def get_urls_from_xls(xls_file_path):
 
     # Iterate through all the values in column A starting at A2
     for row in range(1, sheet.nrows):  # 1 to skip the header
-        value = sheet.cell_value(row, 0)  # 0 refers to column A
-        url = f"https://www.njstart.gov/bso/external/bidDetail.sdo?docId={value}&external=true&parentUrl=close"
-        urlsToScrape.append(url)
+        doc_id = sheet.cell_value(row, 0)  # 0 refers to column A
+        url_to_scrape = f"https://www.njstart.gov/bso/external/bidDetail.sdo?docId={doc_id}&external=true&parentUrl=close"
+        # Appending the object with url_to_scrape and doc_id
+        urls_to_scrape_data.append({"url_to_scrape": url_to_scrape, "doc_id": doc_id})
 
-    return urlsToScrape
+    return urls_to_scrape_data
